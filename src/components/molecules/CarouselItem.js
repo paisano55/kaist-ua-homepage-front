bed163a1f4de8d30729a76b882141d5c3fba6111
ksjae <ksjae0705@hotmail.com>
2022-11-21 21:54:25 +0900

    feat(Livestream): Add channel id

--- src/components/molecules/CarouselItem.js
+++ src/components/molecules/CarouselItem.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 
 const CarouselItem = props => {
-  const carouselItem = (props.alt == "Youtube Livestream") ?
+  const carouselItem = (props.alt == "Youtube Livestream (EMBED)") ?
     <Carousel.Item className={props.className} >
-      <iframe className="d-block w-100" width="100%" height="100%" src="https://www.youtube.com/embed/live_stream?channel=*FILL IN*!!!!!!" frameborder="0" allowfullscreen></iframe>
+      <iframe className="d-block w-100" width="100%" height="100%" src="https://www.youtube.com/embed/live_stream?channel=UCOyNLfCKA5k0PotGj4xTPmg" frameborder="0" allowfullscreen></iframe>
     </Carousel.Item> :
     <Carousel.Item className={props.className} >
       <a href={props.href}>
