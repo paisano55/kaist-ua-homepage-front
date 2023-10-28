0435b9ff5e7dedd207385073f7d1d769e4a17730
paisano55 <paisano55@naver.com>
2023-10-28 18:17:32 +0900

    intro order feature

--- src/components/molecules/introduction/EditableIntroductionList.js
+++ src/components/molecules/introduction/EditableIntroductionList.js
@@ -67,26 +67,24 @@ const EditableIntroductionList = ({ intros }) => {
         const parents = [];
 
         for (let i = 0; i < intros.length; i++) {
-            if (!intros[i].parentId) {
-                if (intros.filter(intro => intro.parentId === intros[i].id).length > 0) {
-                    intros[i].isParent = true;
-                    intros[i].childrenList = []
-                    intros[i].link = `#${intros[i].id}`;
-                    const subList = intros.filter(intro => intro.parentId === intros[i].id);
-                    for (let j = 0; j < subList.length; j++) {
-                        subList[j].isParent = false;
-                        subList[j].subId = j + 1;
-                        subList[j].childrenList = [];
-                        subList[j].link = `#${intros[i].id}_${subList[j].subId}`;
-                        intros[i].childrenList.push(subList[j]);
-                    }
-                    parents.push(intros[i]);
-                } else {
-                    intros[i].isParent = false;
-                    intros[i].childrenList = [];
-                    intros[i].link = `#${intros[i].id}`;
-                    parents.push(intros[i]);
+            if (intros.subIntros.length > 0) {
+                intros[i].isParent = true;
+                intros[i].childrenList = []
+                intros[i].link = `#${intros[i].id}`;
+                const subList = intros[i].subIntros;
+                for (let j = 0; j < subList.length; j++) {
+                    subList[j].isParent = false;
+                    subList[j].subId = j + 1;
+                    subList[j].childrenList = [];
+                    subList[j].link = `#${intros[i].id}_${subList[j].subId}`;
+                    intros[i].childrenList.push(subList[j]);
                 }
+                parents.push(intros[i]);
+            } else {
+                intros[i].isParent = false;
+                intros[i].childrenList = [];
+                intros[i].link = `#${intros[i].id}`;
+                parents.push(intros[i]);
             }
         }
         setParentList(parents);
--- src/components/organisms/IntroEditorHeader.js
+++ src/components/organisms/IntroEditorHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Container, Form } from "react-bootstrap";
 import { EditorFormGroup } from "../molecules";
@@ -9,6 +9,12 @@ const IntroEditorHeader = ({ onChangeField, isEdit, intros }) => {
         onChangeField({ key, value: e.target.value });
     };
     const intro = useSelector(({ intro }) => intro);
+    const [prevIntros, setPrevIntros] = useState(intros);
+
+    const handleParentIdChange = (e) => {
+        onChange("parentId", e);
+        setPrevIntros(intros.filter(intro => intro.parentId === e.target.value));
+    };
 
     return (
         <Container className="border-bottom mt-3">
@@ -25,9 +31,15 @@ const IntroEditorHeader = ({ onChangeField, isEdit, intros }) => {
                 />
 
                 {!isEdit ? <Form.Label>추가할 항목 선택</Form.Label> : <Form.Label>편집 시에는 항목 선택이 불가합니다.</Form.Label>}
-                <select className="form-control" variant="outline-primary" value={intro.parentId} onChange={e => onChange("parentId", e)} disabled={isEdit}>
+                <select className="form-control" variant="outline-primary" value={intro.parentId} onChange={handleParentIdChange} disabled={isEdit}>
                     <option value="None">대항목</option>
                     {intros ? intros.filter(intro => !intro.parentId).map(intro => <option value={intro.id}>"{intro.korTitle}" 의 소항목</option>) : null}
+                </select>
+
+                {!isEdit ? <Form.Label>추가할 위치 선택</Form.Label> : <Form.Label>편집 시에는 항목 선택이 불가합니다.</Form.Label>}
+                <select className="form-control" variant="outline-primary" value={intro.prevId} onChange={e => onChange("prevId", e)} disabled={isEdit}>
+                    {!intros ? <option value="None">첫 대항목 추가</option> : null}
+                    {prevIntros ? prevIntros.map(intro => <option value={intro.id}>"{intro.korTitle}" 뒤에 추가</option>) : <option value="None">첫 소항목 추가</option>}
                 </select>
             </Form>
         </Container>
